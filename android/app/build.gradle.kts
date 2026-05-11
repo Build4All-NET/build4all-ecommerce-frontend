bfecda191374dcb9735f537a482d1d7e12c27047
fatima kassem <fatima.kassem@buil4all.net>
2026-05-11 12:12:45 +0300

    updated

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,12 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("com.google.gms.google-services")
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
 }
-
-import java.util.Properties
-import java.io.FileInputStream
 
 val keystoreProperties = Properties()
 val keystorePropertiesFile = rootProject.file("key.properties")
@@ -16,12 +16,21 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.build4all_ecommerce"
+    namespace = "com.build4all.front"
     compileSdk = 36
     ndkVersion = "27.0.12077973"
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
     defaultConfig {
-        applicationId = "com.example.build4all_ecommerce"
+        applicationId = "com.build4all.opl1.test"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -30,11 +39,13 @@ android {
 
     signingConfigs {
         create("release") {
-            if (keystorePropertiesFile.exists()) {
-                keyAlias = keystoreProperties["keyAlias"] as String?
-                keyPassword = keystoreProperties["keyPassword"] as String?
-                storeFile = keystoreProperties["storeFile"]?.let { file(it as String) }
-                storePassword = keystoreProperties["storePassword"] as String?
+            keyAlias = keystoreProperties.getProperty("keyAlias")
+            keyPassword = keystoreProperties.getProperty("keyPassword")
+            storePassword = keystoreProperties.getProperty("storePassword")
+
+            val storeFilePath = keystoreProperties.getProperty("storeFile")
+            if (!storeFilePath.isNullOrBlank()) {
+                storeFile = file(storeFilePath)
             }
         }
     }
@@ -46,6 +57,10 @@ android {
             isShrinkResources = false
         }
     }
+}
+
+dependencies {
+    implementation("androidx.core:core-splashscreen:1.0.1")
 }
 
 flutter {
